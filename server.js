36ebfe39fd94d08fb92ab3293d2d02a623b06daf
MhmdRhayem <10121011@mu.edu.lb>
2024-07-02 06:02:58 +0300

    Adding body-parser module

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import { dirname } from "path"
 import { fileURLToPath } from "url";
+import bodyParser from 'body-parser';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const API_KEY = 'https://api.blockchain.com/v3/exchange'
 const app = express()
 const port = 3000
 app.use(express.static("public"))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/",(req,res)=>{
     res.render("index.ejs");
